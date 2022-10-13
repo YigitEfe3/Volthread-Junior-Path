c9e2281c0e6b957f50b93b5ebc0b93d585ee224c
YigitEfe3 <efe.yigit.efee@gmail.com>
2022-10-13 10:09:29 +0300

    Deployment Trail completed

---
+++ src/main/java/org/example/Deployment/Applets.java
@@ -0,0 +1,21 @@
+package org.example.Deployment;
+
+/*
+* Applet is a special type of program that is embedded in the webpage to generate the dynamic content.
+*       It runs inside the browser and works at client side.
+*
+* Lifecycle of an applet
+*   - Applet is initialized
+*   - Applet is started
+*   - Applet is painted
+*   - Applet is stopped
+*   - Applet is destroyed
+*
+*       Object -> Component -> Container -> Window -> Panel -> Applet
+* (java.lang)     \-------------- java.awt ---------------/    (java.applet)
+*
+* */
+
+public class Applets{
+
+}
---
+++ src/main/java/org/example/Deployment/JARFiles.java
@@ -0,0 +1,14 @@
+package org.example.Deployment;
+
+/*
+*
+* JAR = Java ARchive
+*       - compressed format of a compiled java project
+*       - contains .class files, metadata and some other resources (not .java files)
+*       - used to distribute java code/libraries for use by other projects
+*       - direct execution of java program (without IDE)
+*
+* */
+
+public class JARFiles {
+}
